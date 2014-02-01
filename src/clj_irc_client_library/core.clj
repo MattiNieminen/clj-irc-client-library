ab04364d340701ab86b1c8e9c6e4791b6bb66911
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2014-02-01 15:25:36 +0200

    Add initial version of connection related functions.

--- src/clj_irc_client_library/core.clj
+++ src/clj_irc_client_library/core.clj
@@ -1,6 +1,41 @@
-(ns clj-irc-client-library.core)
+(ns clj-irc-client-library.core
+  (:import (java.net Socket)
+           (java.io PrintWriter InputStreamReader BufferedReader)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handle-connection
+  "Handles irc connection created by connect. Takes care of PING PONG and
+  writes incoming messages to ref in connection"
+  [connection]
+  (while (nil? (:exit @connection))
+    (let [new-message (.readLine (:in @connection))]
+      (println new-message)
+      (cond
+         (re-find #"^ERROR :Closing Link:" new-message)
+         (dosync (alter connection merge {:exit true}))
+         (re-find #"^PING" new-message)
+         (write connection "PONG " (re-find #":.*" new-message))))))
+
+(defn connect
+  "Connects to IRC server and returns a connection"
+  [hostname port]
+  (let [socket (Socket. hostname port)
+        connection (ref {:in (BufferedReader. (InputStreamReader.
+                                                (.getInputStream socket)))
+                         :out (PrintWriter. (.getOutputStream socket))
+                         :exit nil})]
+    (future (handle-connection connection))
+    connection))
+
+(defn write
+  "Writes message to connection output and flushes."
+  [connection message]
+  (doto (:out @connection)
+    (.println (str message "\r"))
+    (.flush)))
+
+;TODO should there be just one form with do to clearly indicate side effects?
+(defn login
+  "Sends necessary login messages to IRC server."
+  [connection nick user mode real-name]
+  (write connection (str "NICK " nick))
+  (write connection (str "USER " user " " mode " * :" real-name)))
