34effef13892d9a3b318f13bc1976d71032a8787
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2014-02-23 18:14:32 +0200

    Implement handling of names reply when joining to a channel.

--- src/clj_irc_client_library/core.clj
+++ src/clj_irc_client_library/core.clj
@@ -1,7 +1,12 @@
 (ns clj-irc-client-library.core
   (:import (java.net Socket)
            (java.io PrintWriter InputStreamReader BufferedReader))
-  (:use [clojure.string :only [split]]))
+  (:use [clojure.string :only [split trim]]))
+
+(defn words
+  "Splits string by whitespace"
+  [words]
+  (split words #"\s+"))
 
 (defn create-channel
   "Creates a new channel."
@@ -14,6 +19,27 @@
   [connection name channel]
   (dosync (alter connection #(merge-with merge %1 %2)
                  {:channels {(keyword name) channel}})))
+
+(defn add-names-to-channel
+  "Adds list of names /nicks to a channel"
+  [channel names]
+  (let [new-names (set (clojure.set/union (:names channel) names))]
+  (merge channel {:names new-names})))
+
+(defn get-channel-name-from-names-reply
+  "Gets channel name from reply message"
+  [message]
+  (trim (subs (re-find #"[@|=] #.[^\s]*" message) 1)))
+
+(defn get-names-from-names-reply
+  "Gets names as list from reply message"
+  [message]
+  (words (subs (re-find #" :.*" message) 2)))
+
+(defn get-channel-from-connection
+  "Gets channel from connection"
+  [connection channel]
+  (get (:channels connection) (keyword channel)))
 
 (defn write
   "Writes messages to the connection output and flushes."
@@ -28,20 +54,27 @@
   [connection]
   (while (nil? (:exit @connection))
     (let [new-message (.readLine (:in @connection))
-          message-words (split new-message  #"\s+")
-          numeric-reply (second message-words)]
+          message-words (words new-message)
+          reply-code (second message-words)]
       (println new-message)
       (cond
          (re-find #"^ERROR :Closing Link:" new-message)
          (dosync (alter connection merge {:exit true}))
          (re-find #"^PING" new-message)
          (write connection (str "PONG " (re-find #":.*" new-message)))
-         (re-find #"^004" numeric-reply)
+         (re-find #"^004" reply-code)
          (merge-channel-to-connection connection (nth message-words 3)
                                       (create-channel))
-         (re-find #"JOIN" (second message-words))
-         (merge-channel-to-connection connection (nth message-words 2)
-                                      (create-channel))))))
+         (re-find #"JOIN" reply-code)
+         (merge-channel-to-connection
+           connection (re-find #"#.*" (nth message-words 2)) (create-channel))
+         (re-find #"353" reply-code)
+         (merge-channel-to-connection
+           connection (get-channel-name-from-names-reply new-message)
+           (add-names-to-channel
+             (get-channel-from-connection
+               @connection (get-channel-name-from-names-reply new-message))
+             (get-names-from-names-reply new-message)))))))
 
 (defn connect
   "Connects to IRC server and returns the connection."
